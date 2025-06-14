4f7b07c14f42c9d010923f6b457373bf3cc9b2ba
helloworld <shchur.vadim.90@list.ru>
2025-06-14 12:43:31 +0300

    Пожалуйста, проверьте дз.

--- src/Main.java
+++ src/Main.java
@@ -34,52 +34,70 @@ public class Main {
             int newBirths = people * birthRate / 1000;
             int deaths = people * fatality / 1000;
             people = people + newBirths - deaths;
-            System.out.println("Год " + io + ", численность населения составляет " + people);}
+            System.out.println("Год " + io + ", численность населения составляет " + people);
+        }
 
-            //Задача 4;
-            System.out.println("Задача 4");
-            int deposit = 15_000;
-            int depositFinal = 12_000_000;
-            double capital = 0.07;
-            int month = 0;
-            while (deposit < depositFinal)
-                deposit += deposit * capital;
-            month++;{
-            System.out.println("Месяц " + month + ", сумма накоплений равна " + deposit + " рублей");}
-            //Задача 5;
-            System.out.println("Задача 5");
-            int currentAmount = 15_000;
-            int depositFinals = 12_000_000;
-            double capitals = 0.07;
-            int monthCount = 0;
-            while (currentAmount < depositFinals)
-                currentAmount += currentAmount * capitals;
+        //Задача 4;
+        System.out.println("Задача 4");
+        int monthCount = 0;
+        double currentAmount = 15000;
+        int targetAmounts = 12000000;
+
+        while (currentAmount < targetAmount) {
+            currentAmount *= 1.07; // добавляем 7%
             monthCount++;
-            if (monthCount % 6 == 0){
-                System.out.println("Месяц " + monthCount + ", сумма накоплений равна " + currentAmount + " рублей");}
+            System.out.println("Месяц " + monthCount + ", сумма накоплений равна " + currentAmount);
+        }
+        //Задача 5;
+        System.out.println("Задача 5");
+        int monthCounts = 0;
+        double currentAmounts = 15000;
+        int targetAmountss = 12000000;
 
-            //Задача 6;
-            System.out.println("Задача 6");
-            for (int yearse = 1; yearse <= 9; yearse++)
-                for (int monthCounts = 1; monthCounts <= 6; monthCounts++)
-                    currentAmount += currentAmount * capitals;{
-            System.out.println("Полгода " + "yearse" + ", сумма накоплений равна " + currentAmount + "рублей.");}
-            //Задача 7;
-            System.out.println("Задача 7");
-            int firstFriday = 3;
-            for (int day = firstFriday; day <= 31; day += 7){
-                System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");}
+        while (currentAmounts < targetAmountss) {
+            currentAmounts *= 1.07; // добавляем 7%
+            monthCount++;
 
-            //Задача 8;
-            System.out.println("Задача 8");
-            int cometaYear = 0;
-            int ourYear = 2025;
-            int beginning = ourYear - 200;
-            int ending = ourYear + 100;
-            do {
-                cometaYear += 79;
-                if (cometaYear > beginning && cometaYear < ending) {
-                    System.out.println(cometaYear);
-                }
-            } while (cometaYear < ending);
-        }}
+            if (monthCount % 6 == 0) {
+                System.out.println("Месяц " + monthCount + ", сумма накоплений равна " + currentAmounts);
+            }
+        }
+
+        //Задача 6;
+        System.out.println("Задача 6");
+        int monthCountss = 0;
+        int totalMonths = 9 * 12; // 9 лет в месяцах
+        int halfYear = 6; // 6 месяцев
+        int initialAmount = 15000;
+        double currentAmountss = initialAmount;
+
+        while (monthCountss < totalMonths) {
+            currentAmountss *= 1.07; // добавляем 7% каждый месяц
+            monthCountss++;
+
+            // вывод суммы каждые полгода
+            if (monthCountss % halfYear == 0) {
+                System.out.println("Полгода " + (monthCountss / halfYear) + ", сумма накоплений равна " + currentAmountss + " рублей.");
+            }
+        }
+        //Задача 7;
+        System.out.println("Задача 7");
+        int firstFriday = 3;
+        for (int day = firstFriday; day <= 31; day += 7) {
+            System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");
+        }
+
+        //Задача 8;
+        System.out.println("Задача 8");
+        int cometaYear = 0;
+        int ourYear = 2025;
+        int beginning = ourYear - 200;
+        int ending = ourYear + 100;
+        do {
+            cometaYear += 79;
+            if (cometaYear > beginning && cometaYear < ending) {
+                System.out.println(cometaYear);
+            }
+        } while (cometaYear < ending);
+    }
+}
